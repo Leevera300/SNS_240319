0d9d2f457e364f98cc126d48b8719b28a23d0f4c
DESKTOP-SES5PE0\user <test@test.com>
2024-07-17 18:50:09 +0900

    240717_수업내용

---
+++ src/main/java/com/sns/post/PostRestController.java
@@ -0,0 +1,23 @@
+package com.sns.post;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sns.post.bo.PostBO;
+
+@RequestMapping("/post")
+@RestController
+public class PostRestController {
+
+	@Autowired
+	private PostBO postBO;
+	
+	@PostMapping("/create")
+	public Map<String, Object> create() {
+		
+	}
+}
