18cbedf6c7d1f58bb52fdcc30136e33c808b41bb
DESKTOP-SES5PE0\user <test@test.com>
2024-07-17 15:54:31 +0900

    240716_수업내용

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,15 +1,33 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping("/timeline")
 @Controller
 public class TimelineController {
 
+	@Autowired
+	private PostBO postBO;
+	
 	@GetMapping("/timeline-view")
-	public String timelineView() {
+	public String timelineView(Model model) {
+		// 글 조회
+		List<PostEntity> postList = postBO.getPostEntityList();
+		
+		// model 담기
+		model.addAttribute("postList", postList);
+		
 		return "timeline/timeline";
 	}
 }
