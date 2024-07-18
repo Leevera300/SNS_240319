13b3ce57f575b445975d89d5dda343d21edca360
DESKTOP-SES5PE0\user <test@test.com>
2024-07-18 18:50:38 +0900

    240718_수업내용

--- src/main/java/com/sns/post/PostRestController.java
+++ src/main/java/com/sns/post/PostRestController.java
@@ -1,13 +1,18 @@
 package com.sns.post;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sns.post.bo.PostBO;
+
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping("/post")
 @RestController
@@ -17,7 +22,30 @@ public class PostRestController {
 	private PostBO postBO;
 	
 	@PostMapping("/create")
-	public Map<String, Object> create() {
+	public Map<String, Object> create(
+			@RequestParam("content") String content,
+			@RequestParam("file") MultipartFile file,
+			HttpSession session) {
 		
+		
+		
+		//// 글쓴이 번호와 아이디를 session에서 꺼낸다.
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		Map<String, Object> result = new HashMap<>();
+		if (userId == null) {
+			result.put("code", 403);
+			result.put("error_message", "로그인을 해주세요.");
+			return result;
+		}
+		
+		// DB insert
+		postBO.addPost(userId, content, file);
+		
+		// 응답값
+		result.put("code", 200);
+		result.put("result", "성공");
+				
+		return result;
 	}
 }
